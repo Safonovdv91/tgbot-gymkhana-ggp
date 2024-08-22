41c48d3e7a995b37e7dde1d1c1225eb4f7e4ab36
Safonov <Sofik.d@gmail.com>
2024-08-22 23:28:48 +0300

    ruff

--- app/betting/sender.py
+++ app/betting/sender.py
@@ -12,8 +12,7 @@ class BettingMessageSender:
     async def sort_by_delta_time(
         bet_users: list[BetTimeTelegramUser],
     ) -> list[BetTimeTelegramUser]:
-        """
-        Функция получаюoщая список ставок на время и сортирующая их
+        """Функция получаюoщая список ставок на время и сортирующая их
         по разнице времени ставки и лучшего времени
         """
         ms = DbStageResults().get_best_stage_time()
@@ -22,8 +21,7 @@ class BettingMessageSender:
             user.delta_bet_time1 = abs(user.bet_time1 - ms)
             return user.delta_bet_time1
 
-        sorted_users = sorted(bet_users, key=time_differense)
-        return sorted_users
+        return sorted(bet_users, key=time_differense)
 
     @staticmethod
     async def get_all_bets() -> str:
@@ -40,10 +38,11 @@ class BettingMessageSender:
     async def get_sorted_bets() -> str:
         bet_users = DbBetTime().get(tg_id="all")
         bet_users = await BettingMessageSender.sort_by_delta_time(bet_users)
-        text = "Сортированные ставки:\n"
+        text = "Таблица ставок на время:\n"
         for i in range(len(bet_users)):
             text += (
                 f"\t{i + 1}) {bet_users[i].tg_user.username}"
-                f" - {BotFunction.msec_to_mmssms(bet_users[i].bet_time1)} [{bet_users[i].delta_bet_time1}мс]\n"
+                f" - {BotFunction.msec_to_mmssms(bet_users[i].bet_time1)} "
+                f"[{bet_users[i].delta_bet_time1}мс]\n"
             )
         return text
